74eac5f6594a1f56c7a9b299a97b3dde3d8c0ec5
Prem07077 <125694672+Prem07077@users.noreply.github.com>
2024-08-29 17:33:03 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,41 +1,31 @@
-# Shared configuration for edX docs.
-
 import datetime
 import os
 import sys
 from pathlib import Path
 
-
-# What release line is this? Use "master" for master, and the release name
-# on release branches. Zebrawood should have "zebrawood".
+# Define the release line and project version
 release_line = "master"
-
-# The slug that is used by ReadTheDocs for this version of the projects.
 project_version = "latest" if (release_line == "master") else f"open-release-{release_line}.master"
 
-# on_rtd is whether we are on readthedocs.io, this line of code grabbed from docs.readthedocs.io
+# Check if we are on ReadTheDocs
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-# OK, this is gross: I don't know of a way to find out what builder is running,
-# so let's examine the command line, and take the word after -b.
+# Determine the builder
 try:
     the_builder = sys.argv[sys.argv.index("-b") + 1]
 except ValueError:
     the_builder = None
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# Add the path to the en_us directory
+source_dir = os.path.join(Path.cwd(), 'en_us')
+sys.path.insert(0, source_dir)
 
+# Sphinx configuration
 master_doc = 'index'
-
-# The suffix of source filenames.
 source_suffix = '.rst'
-
-# The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
-# Add any Sphinx extension module names here, as strings. They can be extensions
-# coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
+# Sphinx extensions
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
@@ -48,14 +38,8 @@ extensions = [
     'sphinx_reredirects',
 ]
 
-# The encoding of source files.
-# source_encoding = 'utf-8-sig'
-
-# The master toctree document.
-master_doc = 'index'
-
+# HTML output options
 html_context = {}
-
 if on_rtd:
     html_context["on_rtd"] = on_rtd
     html_context["google_analytics_id"] = ''
@@ -66,7 +50,6 @@ if on_rtd:
 html_theme = 'sphinx_book_theme'
 path_to_docs = '/'.join(Path.cwd().parts[-3:])
 
-# html_theme_path = []
 html_theme_options = {
     "repository_url": "https://github.com/Prem07077/edx-documentation",
     "repository_branch": "master",
@@ -76,7 +59,6 @@ html_theme_options = {
     "use_issues_button": True,
     "use_edit_page_button": True,
     "navigation_with_keys": False,
-    # Please don't change unless you know what you're doing.
     "extra_footer": """
         <a rel="license" href="https://creativecommons.org/licenses/by-sa/4.0/">
             <img
@@ -104,17 +86,10 @@ html_theme_options['navigation_depth'] = 3
 html_logo = "https://logos.openedx.org/open-edx-logo-color.png"
 html_favicon = "https://logos.openedx.org/open-edx-favicon.ico"
 
-# Help and Feedback links. These are customized for the category and audience
-# of the book. Add a line to the book's conf.py like this:
-#
-# set_audience(PARTNER, COURSE_TEAMS)
-#
-
-# Categories
+# Help and Feedback links
 PARTNER = object()
 OPENEDX = object()
 
-# Audiences
 COURSE_TEAMS = object()
 LEARNERS = object()
 RESEARCHERS = object()
@@ -122,7 +97,7 @@ DEVELOPERS = object()
 
 HELP_LINKS = {
     (PARTNER, COURSE_TEAMS): {
-        'help_url': None, #"https://partners.edx.org/forums/partner-forums",
+        'help_url': None,
         'help_link_text': None,
     },
     (PARTNER, LEARNERS): {
@@ -151,7 +126,6 @@ HELP_LINKS = {
     },
 }
 
-# Defaults for the help links.
 html_context.update({
     'help_url': None,
     'help_link_text': None,
@@ -164,27 +138,10 @@ def set_audience(category, audience):
     if help_data:
         html_context.update(help_data)
 
-# General information about the project.
-
-print(html_context)
-
+# General information about the project
 copyright = '{year}, The Axim Collaborative'.format(year=datetime.datetime.now().year)
 
-# Intersphinx manages the connections between books.
-# Normally the references in a book are downloaded from readthedocs. But you
-# can also provide a local file to look in. It's easier to fix broken
-# references between books if you can do a local build and use the local
-# reference files. These helper functions are for both reducing the repetition
-# in the mapping dictionary, and for optionally specifying a local file to look
-# in if it exists.
-#
-# We often use the same directory to build two books (edX vs Open edX). In
-# those cases, only use ism_location for one book, not both, or we'll be
-# looking for A's references in an index built for B.
-#
-# openedx_rtd_url is for books that are branched and built for specific Open
-# edX releases. edx_rtd_url is for books that are not.
-
+# Intersphinx mapping
 def edx_rtd_url(slug):
     """Make a RTD URL for a book that doesn't branch for each release."""
     return f"https://edx.readthedocs.io/projects/{slug}/en/latest/"
@@ -194,11 +151,7 @@ def openedx_rtd_url(slug):
     return f"https://edx.readthedocs.io/projects/{slug}/en/{project_version}/"
 
 def ism_location(dir_name):
-    """Calculate the intersphinx_mapping location to use for a book.
-
-    `dir_name` is the directory name under edx-documentation/en_us for the book.
-
-    """
+    """Calculate the intersphinx_mapping location to use for a book."""
     objects_inv = f"../../{dir_name}/build/html/objects.inv"
     if os.path.exists(objects_inv):
         return (objects_inv, None)
